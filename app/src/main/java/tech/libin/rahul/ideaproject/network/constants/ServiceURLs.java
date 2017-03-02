19b8209deb1f2c7d49fe6949e6c3df5d4d73693b
rahulrajptz <rahulrajptz@yahoo.com>
2017-03-02 18:11:39 +0530

    Login completed

--- app/src/main/java/tech/libin/rahul/ideaproject/network/constants/ServiceURLs.java
+++ app/src/main/java/tech/libin/rahul/ideaproject/network/constants/ServiceURLs.java
@@ -6,11 +6,12 @@ package tech.libin.rahul.ideaproject.network.constants;
 
 public class ServiceURLs {
     public static final String REGISTER = "/user/register";
-    public static final String LOGIN = "/postLoginJson";
+    public static final String LOGIN = "/Account/Login";
+    public static final String ACTIVITY = "/AppData/Activity";
     public static final String UPDATE_USER_PROFILE = "/user/updateprofile";
     public static final String UPDATE_PROFILE_PIC = "/saveProfilePic";
 
-    private static final String BASE_URL = "http://192.9.200.168/UrlCallerTester/Service1.svc";
+    private static final String BASE_URL = "http://122.166.96.206/isft/api";
 
     public static String build(String subURL) {
         return BASE_URL + subURL;
